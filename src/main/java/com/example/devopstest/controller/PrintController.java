24068603c4cbcc573bdbab28dc8a0c316f2374bd
sovanseyha <yan.sovanseyha@gmail.com>
2023-09-14 18:06:13 +0000

    msg

--- src/main/java/com/example/devopstest/controller/PrintController.java
+++ src/main/java/com/example/devopstest/controller/PrintController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1111/")
 public class PrintController {
     @GetMapping("/hibroooo")
     public ResponseEntity<String> saySomething(@RequestParam String words){
