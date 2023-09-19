b1e2c8c64a392288d52d7a0c663f89451529a95a
sovanseyha <yan.sovanseyha@gmail.com>
2023-09-19 02:54:21 +0000

    msg

--- src/main/java/com/example/devopstest/controller/PrintController.java
+++ src/main/java/com/example/devopstest/controller/PrintController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v2000/")
 public class PrintController {
-    @GetMapping("/somanang")
+    @GetMapping("/យ៉ាន សុវណ្ណសីហា")
     public ResponseEntity<String> saySomething(@RequestParam String words){
         return ResponseEntity.ok(words);
     }
